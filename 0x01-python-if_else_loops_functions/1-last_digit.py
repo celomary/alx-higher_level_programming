9c8dc1940e2745a91d2db072efbff4aa69adcea8
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-06 22:58:22 +0100

    feat: add space before starting of the second statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ print("", last, end="")
 if last == 0:
     print(" and is 0")
 elif last > 5:
-    print("and is greater than 5")
+    print(" and is greater than 5")
 else:
-    print("and is less than 6 and not 0")
+    print(" and is less than 6 and not 0")
