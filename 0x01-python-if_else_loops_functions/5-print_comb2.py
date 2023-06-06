913f0ca7b9d2334dd6b30c66620a37d731b07f74
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-06 23:10:25 +0100

    chore: modify some function to meet the requirement

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,4 @@
 for i in range(0, 10):
     for j in range(0, 10):
         print('{}{}'.format(i, j), end=', ' if j + 1 < 10 else '')
-    print(end=', ' if i + 1 < 10 else '')
-print()
+    print(end=', ' if i + 1 < 10 else '\n')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if j + 1 < 10 else "")
-    print(end=", " if i + 1 < 9 else "")
-print()
+        print("{}{}".format(i, j), end=", " if j + 1 < 10 else "")
+    print(end=", " if i + 1 < 9 else "\n")
