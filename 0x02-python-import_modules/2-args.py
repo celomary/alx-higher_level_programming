451a8b59c9ee916de16e6135e7ba8dfef8f8b09e
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-08 21:03:17 +0100

    fix: change exercises to meet the requirements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,10 @@ from sys import argv
 
 if __name__ == '__main__':
     args = argv[1:]
-    print("{} argument{}{}".format(len(args), 's' if len(args) != 1 else '', ':' if len(args) else '.'))
+    print("{} argument{}{}".format(
+        len(args),
+        's' if len(args) != 1 else '',
+        ':' if len(args) else '.')
+        )
     for index, arg in enumerate(args):
         print("{}: {}".format(index+1, arg))
