8fd5b385005ef5aca9eae71e1714e65df14b3d68
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-10 16:07:02 +0100

    chore: using index instead of reversing the list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if not my_list:
-        print()
-    for num in my_list[::-1]:
-        print("{:d}".format(num))
+    for idx in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[idx]))
