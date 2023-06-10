dcb9d841739309e836ff9d06eb3bdb8d8c346936
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-10 15:57:05 +0100

    chore: printing new line at the end of outer loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col_idx in range(len(row)):
             print("{}".format(row[col_idx]),
-                  end=" " if col_idx + 1 < len(row) else '\n')
+                  end=" " if col_idx + 1 < len(row) else '')
+        print()
