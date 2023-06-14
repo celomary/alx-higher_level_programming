bd938164a948dc5c72475ab4ac811e04490079d3
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-14 17:40:32 +0100

    style: fix linting problems

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if not a_dictionary or not len(a_dictionary):
         return
-    return max(a_dictionary.items(), key= lambda x: x[1])[0]
+    return max(a_dictionary.items(), key=lambda x: x[1])[0]
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary == None:
+    if not a_dictionary:
         return
     for key in sorted(a_dictionary.keys()):
         print("{}: {}".format(key, a_dictionary[key]))
